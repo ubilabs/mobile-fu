3d0e6b7448c51ec2e50adc46a28d02f81f73504a
Sebastian Korfmann <korfmann@ubilabs.net>
2010-02-19 18:34:33 +0100

    now able to force test-mode in :mobile and :touch

--- lib/mobile_fu.rb
+++ lib/mobile_fu.rb
@@ -34,11 +34,11 @@ module ActionController
       #      has_mobile_fu(true)
       #    end
         
-      def has_mobile_fu(test_mode = false)
+      def has_mobile_fu(test_mode = false, format = :mobile)
         include ActionController::MobileFu::InstanceMethods
 
-        if test_mode 
-          before_filter :force_mobile_format
+        if test_mode
+          before_filter "force_#{format}_format"
         else
           before_filter :set_mobile_format
         end
@@ -65,23 +65,30 @@ module ActionController
     
     module InstanceMethods
       
-      # Forces the request format to be :mobile
+      # Forces the request format to be touch
       
-      def force_mobile_format
-        request.format = :mobile
-        session[:mobile_view] = true if session[:mobile_view].nil?
+      def force_touch_format
+        request.format = :touch
+        session[:touch_view] = '1' if session[:touch_view].nil?
       end
+      
+      # Forces the request format to be mobile
+      def force_mobile_format
+         request.format = :mobile
+         session[:mobile_view] = '1' if session[:mobile_view].nil?
+       end
       
       # Determines the request format based on whether the device is mobile, touch or if
       # the user has opted to use either the 'Standard' view or 'Mobile' view.
       
       def set_mobile_format
+        %w(touch mobile).each {|t| session["#{t}_view".to_sym] = params["#{t}_view".to_sym]}
         if is_touch_device?
-          request.format = session[:touch_view] == false ? :html : :touch
-          session[:touch_view] = true if session[:touch_view].nil?
+          request.format = session[:touch_view] == '0' ? :html : :touch
+          session[:touch_view] = '1' if session[:touch_view].nil?
         elsif is_mobile_device?
-          request.format = session[:mobile_view] == false ? :html : :mobile
-          session[:mobile_view] = true if session[:mobile_view].nil?
+          request.format = session[:mobile_view] == '0' ? :html : :mobile
+          session[:mobile_view] = '1' if session[:mobile_view].nil?
         end
       end
       
@@ -102,7 +109,7 @@ module ActionController
          # Returns either true or false depending on whether or not the user agent of
           # the device making the request is matched to a touch-device in our regex.
       def is_touch_device?
-        request.user_agent.to_s.downcase =~ Regexp.new(ActionController::MobileFu::TOUCH_USER_AGENTS)
+        request.user_agent.to_s.downcase =~ Regexp.new(ActionController::MobileFu::TOUCH_USER_AGENTS) 
       end
       
       # Returns either true or false depending on whether or not the user agent of
